f7f2bc76f8ced4240e8a94b9c24566f97fc94510
Ezequiel <ezequielrb7@gmail.com>
2019-12-12 00:32:17 -0300

    instalando configuracoes e limpando o projeto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="background-home">
+    
+  </v-container>
+</template>
+
+<script>
+
+  
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped = css>
+.background-home{
+  background-image: './assets/images/43688.jpg'
+}
+</style>
